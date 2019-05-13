df6d2433bc42c39ab36fd0c61c95c17c1c4740a2
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2019-05-13 11:12:33 +1000

    [.travis.yml] Fixed `cargo test` task; [*.rs] rustfmt --edition=2018

--- bin/lachesis_tcp.rs
+++ bin/lachesis_tcp.rs
@@ -1,14 +1,13 @@
 #[macro_use]
 extern crate structopt;
-extern crate liblachesis_lachesis_rs;
 extern crate libcli;
+extern crate liblachesis_lachesis_rs;
 
-use liblachesis_lachesis_rs::tcp_server::{TcpApp, TcpNode, TcpPeer};
 use libcli::LogOptions;
+use liblachesis_lachesis_rs::tcp_server::{TcpApp, TcpNode, TcpPeer};
 use std::env::args;
 use std::sync::Arc;
 use structopt::StructOpt;
-
 
 const BASE_PORT: usize = 9000;
 const USAGE: &'static str = "Usage: tcp-client [number of nodes] [consensus-algorithm]";
@@ -20,40 +19,40 @@ struct Opt {
     #[structopt(long = "datadar", parse(from_os_str))]
     /// Top-level directory for configuration and data
     data_dir: Option<String>,
-    
+
     #[structopt(long = "log")]
     /// Log level: debug, info, warn, error, fatal, panic
     log: Option<LogOptions>,
 
-    #[structopt(long = "log2file", default_value="false")]
+    #[structopt(long = "log2file", default_value = "false")]
     /// duplicate log output into file lachesis_<BindAddr>.log
     log2file: bool,
 
-    #[structopt(long = "syslog", default_value="false")]
+    #[structopt(long = "syslog", default_value = "false")]
     /// duplicate log output into syslog
     syslog: bool,
 
-    #[structopt(long = "pidfile", parse(from_os_str), default_value="/tmp/false")]
+    #[structopt(long = "pidfile", parse(from_os_str), default_value = "/tmp/false")]
     /// pidfile location; /tmp/rs-lachesis.pid by default
     syslog: Option<String>,
-    
-    #[structopt(name="listen", long, short)]
+
+    #[structopt(name = "listen", long, short)]
     /// Listen IP:Port for lachesis node
     listen: Option<String>,
 
-    #[structopt(name="timeout", long, short, default_value="180")]
+    #[structopt(name = "timeout", long, short, default_value = "180")]
     /// TCP timeout in seconds
     timeout: i64,
 
-    #[structopt(name="proxy-listen", short, long)]
+    #[structopt(name = "proxy-listen", short, long)]
     /// Listen IP:Port for lachesis proxy
     proxy_listen: Option<String>,
 
-    #[structopt(name="heartbeat", long, default_value="5")]
+    #[structopt(name = "heartbeat", long, default_value = "5")]
     /// Time between gossips
     heartbeat: i64,
 
-    #[structopt(name="peer-selector", long, default_value="random")]
+    #[structopt(name = "peer-selector", long, default_value = "random")]
     peer_selector: PeerSelectorOptions,
 }
 /**
